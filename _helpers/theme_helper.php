05d5dcbaff689df333595a9b7ac9c177bd0c3293
pruthvitechsolutions <pruthvitechsolutions@gmail.com>
2020-05-16 13:52:54 +0530

    menu bar created

--- _helpers/theme_helper.php
+++ _helpers/theme_helper.php
@@ -2,3 +2,4 @@
 
 $APP_NAME = "JOB Search UK";
 $APP_MASTER_CSS = "http://localhost/bepractical/project/css/master.css";
+$APP_THEME_CSS = "http://localhost/bepractical/project/css/theme.css";
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include('./_helpers/_helper.php'); ?>
+<?php include('./_helpers/theme_helper.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +6,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $APP_MASTER_CSS; ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo $APP_THEME_CSS; ?>" type="text/css">
     <title> <?php echo $APP_NAME; ?> </title>
 </head>
 
 <body>
-
+    <div class="top-bar">
+        <div class="size">
+            <button>a--</button>
+            <button>a-</button>
+            <button>a+</button>
+            <button>a++</button>
+        </div>
+    </div>
+    <nav class="menu">
+        <a href="/index.php" class="logo"> <?php echo $APP_NAME; ?> </a>
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="jobseeker.php">Job seeker</a>
+                </li>
+                <li>
+                    <a href="recruiter.php">Recruiter</a>
+                </li>
+                <li>
+                    <a href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </body>
 
 </html>
