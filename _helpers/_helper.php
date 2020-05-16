893c502ae0743bcc97daa185f207ad2affc0df6f
pruthvitechsolutions <pruthvitechsolutions@gmail.com>
2020-05-16 12:47:27 +0530

    added master css, & helper

---
+++ _helpers/_helper.php
@@ -0,0 +1,4 @@
+<?php
+
+$APP_NAME = "JOB Search UK";
+$APP_MASTER_CSS = "http://localhost/bepractical/project/css/master.css";
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('./_helpers/_helper.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo $APP_MASTER_CSS; ?>" type="text/css">
+    <title> <?php echo $APP_NAME; ?> </title>
+</head>
+
+<body>
+
+</body>
+
+</html>
